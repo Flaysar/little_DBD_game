7eddf1cb0539e1e098e6d1246e4b847257a3c7a1
Dmitry Medvedev <144724488+DmitryRazumovski@users.noreply.github.com>
2025-05-09 20:19:22 +0300

    Update project.clj

--- server/project.clj
+++ server/project.clj
@@ -1,6 +1,7 @@
 (defproject maniac-game-server "0.1.0-SNAPSHOT"
   :description "Maniac Game Server"
-  :dependencies [[org.clojure/data.json "2.4.0"]
-                 [org.clojure/clojure "1.10.3"]]
-  :main maniac-game-server.core  ; <- Это ключевая строка!
-  :aot [maniac-game-server.core])
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/data.json "2.4.0"]]
+  :main maniac-game-server.core
+  :aot [maniac-game-server.core]
+  :repl-options {:init-ns maniac-game-server.core})
